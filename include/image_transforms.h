76441ec84723ea95e5af3d11b30ae5f61c60c71b
Anastasia <>
2026-05-30 11:11:07 +0300

    final

--- include/image_transforms.h
+++ include/image_transforms.h
@@ -1,8 +1,5 @@
-#pragma once
-
 #include <vector>
 #include <cstdint>
-#include <algorithm>
 
 #include "colors.h"
 #include "compressor_funcs.h"
@@ -28,13 +25,4 @@ void toGrayscale(CompressedImage& img);
 
 template <typename Image>
 void mirror(Image& img, bool horizontal = false) {
-    if (horizontal) {
-        // Flip left-right: reverse each row
-        for (auto& row : img.image_data) {
-            std::reverse(row.begin(), row.end());
-        }
-    } else {
-        // Flip top-bottom: reverse the rows
-        std::reverse(img.image_data.begin(), img.image_data.end());
-    }
 }
